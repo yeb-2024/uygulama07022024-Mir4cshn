b0d4891ee2227170c9ea316bb0967a6e063ad4c4
Mir4cshn <159525962+Mir4cshn@users.noreply.github.com>
2025-02-07 18:48:53 +0300

    profile label eklendi ,dispose eklendi falan filan

--- GitProject/src/AnaSayfa.java
+++ GitProject/src/AnaSayfa.java
@@ -23,6 +23,7 @@ public class AnaSayfa implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
               if(e.getSource() == button){
+                  frame.dispose();
                  ProfilSayfası profilSayfası = new ProfilSayfası();
               }
     }
--- GitProject/src/ProfilSayfası.java
+++ GitProject/src/ProfilSayfası.java
@@ -3,12 +3,17 @@ import java.awt.*;
 
 public class ProfilSayfası {
     JFrame frame = new JFrame();
-
+    JLabel label = new JLabel();
     public ProfilSayfası(){
+        label.setText("İsim : Miraç Şahin");
+        label.setFont(new Font(null, Font.PLAIN, 25));
+
+        frame.add(label);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Profil Sayfası");
-        frame.setLayout(null);
+        frame.setLayout(new GridLayout(3,1));
         frame.setVisible(true);
     }
 }
