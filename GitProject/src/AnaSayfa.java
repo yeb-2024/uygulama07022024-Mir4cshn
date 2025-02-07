55591f3b09a1c42fe202c345948bf91b9d668729
Mir4cshn <159525962+Mir4cshn@users.noreply.github.com>
2025-02-07 18:29:18 +0300

    AnaSayfa oluşturuldu

---
+++ GitProject/src/AnaSayfa.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class AnaSayfa {
+    JFrame frame = new JFrame();
+
+    public AnaSayfa(){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("AnaSayfa");
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
