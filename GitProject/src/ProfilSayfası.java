de478f2932ec89a63f5d0244f64b898754b4dfef
Mir4cshn <159525962+Mir4cshn@users.noreply.github.com>
2025-02-07 19:02:41 +0300

    Resim eklendi açıklamayı eklemedim sıkıntı olur mu

--- GitProject/src/ProfilSayfası.java
+++ GitProject/src/ProfilSayfası.java
@@ -4,11 +4,20 @@ import java.awt.*;
 public class ProfilSayfası {
     JFrame frame = new JFrame();
     JLabel label = new JLabel();
+    JLabel resim = new JLabel();
+
     public ProfilSayfası(){
         label.setText("İsim : Miraç Şahin");
         label.setFont(new Font(null, Font.PLAIN, 25));
 
+        ImageIcon icon = new ImageIcon("Ekmek.png");
+
+        resim = new JLabel(icon);
+
+        frame.add(resim);
         frame.add(label);
+
+
 
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
